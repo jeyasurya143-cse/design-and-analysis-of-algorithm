31bc556bbe60b39d5bfba4e5ed22d2930215a6d9
jeyasurya143-cse <jeyasurya887085@gmail.com>
2026-04-13 10:24:36 +0530

    Create BubbleSort.c

---
+++ C Language/BubbleSort.c
@@ -0,0 +1,28 @@
+//bubble sort using C
+
+#include<stdio.h>
+int main(){
+   int n,i,j,min,temp;
+   printf("enter no of elements:");
+   scanf("%d",&n);
+   int a[n];
+   printf("enter array elements:");
+   for(i=0;i<n;i++){
+      scanf("%d",&a[i]);
+   }
+   for(i=0;i<n-1;i++){
+      for(j=i+1;j<n;j++){
+         if(a[i]>a[j]){
+            temp=a[i];
+            a[i]=a[j];
+            a[j]=temp;
+         }
+      }
+   }
+   printf("sorted using bubble sort:");
+   for(i=0;i<n;i++){
+      printf("%d \t",a[i]);
+    }
+   printf("\n");
+   return 0;
+}
